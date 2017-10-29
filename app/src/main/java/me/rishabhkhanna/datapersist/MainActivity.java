698a8c531128e9cdcb5ec5f3b1615e9a4670382e
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-29 11:31:20 +0530

    initial commit
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/me/rishabhkhanna/datapersist/MainActivity.java
@@ -0,0 +1,45 @@
+package me.rishabhkhanna.datapersist;
+
+import android.content.SharedPreferences;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    SharedPreferences sharedPreferences;
+    public static final String TAG = "MAinActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        sharedPreferences = getSharedPreferences("PREFS",MODE_PRIVATE);
+        editText.setText(sharedPreferences.getString("key","not saved"));
+
+
+        Log.d(TAG, "Data Dir " + Environment.getDataDirectory().getAbsolutePath());
+        Log.d(TAG, "Root Dir " + Environment.getRootDirectory().getAbsolutePath());
+        Log.d(TAG, "Cache Dir " + Environment.getDownloadCacheDirectory().getAbsolutePath());
+        Log.d(TAG, "External Dir " + Environment.getExternalStorageDirectory().getAbsolutePath());
+        Log.d(TAG, "External Public Dir " + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath());
+
+        Log.d(TAG, "onCreate: ------------------------------------------------");
+        Log.d(TAG, "File Dir" + getFilesDir());
+        Log.d(TAG, "Cache Dir" + getCacheDir());
+        Log.d(TAG, "External Dir" + getExternalFilesDir(Environment.DIRECTORY_MUSIC));
+
+
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sharedPreferences.edit().putString("key",editText.getText().toString()).apply();
+    }
+}
