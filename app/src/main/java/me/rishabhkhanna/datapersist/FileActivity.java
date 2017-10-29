f514895ba06827ea8a98433ea832dd56ca5e4470
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-29 12:49:20 +0530

    file write
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/datapersist/FileActivity.java
+++ app/src/main/java/me/rishabhkhanna/datapersist/FileActivity.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileActivity extends AppCompatActivity {
     EditText etFile;
     Button btnStore;
@@ -26,5 +31,14 @@ public class FileActivity extends AppCompatActivity {
 
     public void writeToFile(String fileName, String text){
         // write code to store text in file
+        File path = getFilesDir();
+        File fileToBeStored = new File(path,fileName);
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(fileToBeStored);
+            fileOutputStream.write(text.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
