1317bc50b623e29ea6759f99a1f24f84e8a95aeb
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-29 12:04:19 +0530

    all directories
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/datapersist/MainActivity.java
+++ app/src/main/java/me/rishabhkhanna/datapersist/MainActivity.java
@@ -1,11 +1,14 @@
 package me.rishabhkhanna.datapersist;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -30,9 +33,14 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: ------------------------------------------------");
         Log.d(TAG, "File Dir" + getFilesDir());
         Log.d(TAG, "Cache Dir" + getCacheDir());
-        Log.d(TAG, "External Dir" + getExternalFilesDir(Environment.DIRECTORY_MUSIC));
+        Log.d(TAG, "External Dir" + getExternalFilesDir(null));
 
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            File[] file = getExternalFilesDirs(null);
+            for( File f : file){
+                Log.d(TAG, "External storage " + file);
+            }
+        }
 
 
     }
