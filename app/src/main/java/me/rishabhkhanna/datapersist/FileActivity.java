650b0d30ac9cfe438051a96a23ea4045cded83ad
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-29 13:01:25 +0530

    read file empty method
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/datapersist/FileActivity.java
+++ app/src/main/java/me/rishabhkhanna/datapersist/FileActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,12 +15,14 @@ import java.io.IOException;
 public class FileActivity extends AppCompatActivity {
     EditText etFile;
     Button btnStore;
+    TextView tvRead;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file);
         etFile = (EditText) findViewById(R.id.etFile);
         btnStore = (Button) findViewById(R.id.btnStore);
+        tvRead = (TextView) findViewById(R.id.tvRead);
         btnStore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,7 +30,15 @@ public class FileActivity extends AppCompatActivity {
                 writeToFile("MYFILE",toBeStored);
             }
         });
+
+        readFile("MYFILE");
     }
+
+    public void readFile(String fileName){
+        // write code to read file
+        // set text to tvRead
+    }
+
 
     public void writeToFile(String fileName, String text){
         // write code to store text in file
